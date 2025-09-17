f4fbe3370cc245009a658b5e69e7871db31017bf
rosme-ui <rosmeryachulla@hotmail.com>
2025-09-16 22:27:47 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,104 +1,85 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace empezando
+namespace trabajo_T1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Datos del producto y del cliente
-
-            double descuento = 0;
-
-            string tipoDecliente = "", porcenDes = "";
+            double precioFinal, IGV, precioConDes, montoDesc, precioB, descuento = 0;
+            int Tcliente;
+            string nombre, tipoDecliente = "", porcenDes = "";
 
             Console.WriteLine("====================================");
             Console.WriteLine("       SISTEMA DE FACTURACION       ");
             Console.WriteLine("====================================");
             Console.WriteLine("Ingrese el nombre del producto que desea comprar: ");
-            string nombre = Console.ReadLine();
+            nombre = Console.ReadLine();
+            //Valida que el nombre ingresado esté escrito de manera correcta 
+            if (nombre == ""||!nombre.Any(char.IsLetter))
+            {
+                Console.WriteLine("ERROR!");
+                return;
+            }
             Console.WriteLine("Ingrese el precio del producto: S/ ");
-            double precioB = Convert.ToDouble(Console.ReadLine());
+            precioB = double.Parse(Console.ReadLine());
+            //Valida que el precio ingresado sea el correcto
+            if (precioB<=0)
+            {
+                Console.WriteLine("ERROR!");
+                return;
+            }
             Console.WriteLine("tipo de cliente (1=Frecuente, 2=Nuevo, 3=Corporativo)");
-            int Tcliente = Convert.ToInt32(Console.ReadLine());
+            Tcliente = int.Parse(Console.ReadLine());
             switch (Tcliente)
             {
-
                 case 1:
-
                     descuento = 0.10;
-
                     tipoDecliente = "Frecuente";
-
                     porcenDes = "10%";
-
                     break;
 
                 case 2:
-
                     descuento = 0.00;
-
                     tipoDecliente = "Nuevo";
-
                     porcenDes = "0%";
-
                     break;
 
                 case 3:
-
                     descuento = 0.15;
-
                     tipoDecliente = "Corporativo";
-
                     porcenDes = "15%";
-
                     break;
 
                 default:
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-
                     Console.WriteLine("Cliente no valido ERROR ");
-
-                    Console.ResetColor();
-
                     return;
-                }
-
+            }
 
             //calculos del producto
-
-            double montoDesc = precioB * descuento;
-
-            double precioConDes = precioB - montoDesc;
-
-            double IGV = precioConDes * 0.18;
-
-            double precioFinal = precioConDes + IGV;
+            montoDesc = precioB * descuento;
+            precioConDes = precioB - montoDesc;
+            IGV = precioConDes * 0.18;
+            precioFinal = precioConDes + IGV;
 
             //Resultados finales del producto
-
-            Console.ForegroundColor = ConsoleColor.Green;
-
             Console.WriteLine("====================================");
             Console.WriteLine("              FACTURA               ");
             Console.WriteLine("====================================");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine($"Producto: {nombre}");
-            Console.WriteLine($"Cliente: {tipoDecliente} (Descuento: {porcenDes})");
-            Console.ResetColor();
+            Console.WriteLine($"Cliente: {tipoDecliente} (Descuento Aplicado: {porcenDes})");
             Console.WriteLine("-----------------------------------");
-            Console.WriteLine($"Precio base: S/. {precioB:F2}");
-            Console.WriteLine($"Descuento aplicado: S/. {montoDesc: F2}");
-            Console.WriteLine($"IGV (18%): S/. {IGV:F2}");
-            Console.WriteLine($"Precio final: S/. {precioFinal:F2}");
-            Console.ResetColor();
+            Console.WriteLine($"Precio base: S/. {precioB.ToString("F2")}");
+            Console.WriteLine($"Precio con descuento: S/. {precioConDes.ToString("F2")}");
+            Console.WriteLine($"IGV (18%): S/. {IGV.ToString("F2")}");
+            Console.WriteLine($"Precio final: S/. {precioFinal.ToString("F2")}");
 
-        }
-    }
+            Console.ReadKey();
+        }
+    }
 }
